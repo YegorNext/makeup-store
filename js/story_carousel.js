ea1cc1e01c344018c205dbfada4bb32794701603
YegorNext <yegor.d2015@gmail.com>
2024-03-04 16:21:22 +0200

    Fixes v7

--- js/story_carousel.js
+++ js/story_carousel.js
@@ -2,24 +2,44 @@ const dragBox = document.querySelector(".history__content-wrap");
 const scrollSpeed = 0.85; 
 
 let isDragging = false;
-let startX;
+let startTouchX;
+
+const startDragging = (e) => {
+    if (e.type === 'touchstart') {
+        isDragging = true;
+        startTouchX = e.touches[0].clientX;
+    } else if (e.type === 'mousedown') {
+        isDragging = true;
+        startTouchX = e.clientX;
+    }
+}
 
 const dragging = (e) => {
     if (!isDragging) return;
-    dragBox.scrollLeft = e.type === 'touchmove' ? (startX - e.touches[0].clientX) * scrollSpeed : e.movementX;
-    startX = e.type === 'touchmove' ? e.touches[0].clientX : null;
+
+    let currentTouchX;
+
+    if (e.type === 'touchmove') {
+        currentTouchX = e.touches[0].clientX;
+    } else if (e.type === 'mousemove') {
+        currentTouchX = e.clientX;
+    }
+
+    const movementX = (startTouchX - currentTouchX) * scrollSpeed;
+
+    dragBox.scrollLeft += movementX;
+
+    startTouchX = currentTouchX;
 }
 
-dragBox.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = null;
-});
-document.addEventListener('mouseup', () => isDragging = false);
+const stopDragging = () => {
+    isDragging = false;
+}
+
+dragBox.addEventListener('mousedown', startDragging);
+document.addEventListener('mouseup', stopDragging);
 dragBox.addEventListener('mousemove', dragging);
 
-dragBox.addEventListener('touchstart', (e) => {
-    isDragging = true;
-    startX = e.touches[0].clientX;
-});
+dragBox.addEventListener('touchstart', startDragging);
 dragBox.addEventListener('touchmove', dragging);
-document.addEventListener('touchend', () => isDragging = false);
+document.addEventListener('touchend', stopDragging);
