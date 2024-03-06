fce722b9cb5bcda9aa83b01c7bf9bedac956e0bd
YegorNext <yegor.d2015@gmail.com>
2024-03-06 16:59:40 +0200

    history-carousel changed

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,6 +1,9 @@
 const tabletCarouselElement = document.querySelector('#for-whom-carousel-tablet');
 const mobileCarouselElement = document.querySelector('#for-whom-carousel-mobile');
 
+const tabletCarouselHistoryElement = document.querySelector('#history-carousel-tablet');
+const mobileCarouselHistoryElement = document.querySelector('#history-carousel-mobile');
+
 const carouselTablet = new bootstrap.Carousel(tabletCarouselElement, {
   //interval: 5000
 });
@@ -9,12 +12,28 @@ const carouselMobile = new bootstrap.Carousel(mobileCarouselElement, {
     //interval: 5000
 });
 
+const carouselHistoryTablet = new bootstrap.Carousel(tabletCarouselHistoryElement, {
+  //interval: 5000
+});
+
+const carouselHistoryMobile = new bootstrap.Carousel(mobileCarouselHistoryElement, {
+    //interval: 5000
+});
+
+
+
 function scrollCarousels(){
   carouselTablet.next();
   carouselMobile.next();
 }
 
+function scrollHistoryCarousels(){
+  carouselHistoryTablet.next();
+  carouselHistoryMobile.next();
+}
+
 const intervalTablet = setInterval(scrollCarousels, 5000);
+const intervalHistory = setInterval(scrollHistoryCarousels, 10000);
 
 
 ////
