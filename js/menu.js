be0ac757673068c5df2d3f5e773a6ba4d12f7ec2
root <root@vps-45576>
2024-03-13 12:11:27 +0000

    size-fixes

--- js/menu.js
+++ js/menu.js
@@ -10,15 +10,15 @@ var isEnabled = false;
 function showMenu(){
     isEnabled = true;
 
-    $(overlay).fadeIn(100).addClass('show-menu');
-    $(menuUI).fadeIn(100).addClass('show-menu');
+    $(overlay).fadeIn(50).addClass('show-overlay');
+    $(menuUI).fadeIn(80).addClass('show-menu');
     
     $('html').addClass('overflow-y');
 }
 
 function closeMenu(){
     if(isEnabled){
-        $(overlay).fadeOut(50).removeClass('show-menu');
+        $(overlay).fadeOut(50).removeClass('show-overlay');
         $(menuUI).fadeOut(50).removeClass('show-menu');
 
         $('html').removeClass('overflow-y');
