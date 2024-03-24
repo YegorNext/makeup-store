de8640d436cde9c361f4b2e62e2696affa04c31b
YegorNext <yegor.d2015@gmail.com>
2024-03-24 15:38:03 +0200

    minifier-tools

---
+++ tools/builder.bat
@@ -0,0 +1,2 @@
+@echo off
+type "%~dp0..\css\main.css" "%~dp0..\css\fonts.css" "%~dp0..\css\header.css" "%~dp0..\css\content.css" "%~dp0..\css\for-wh.css" "%~dp0..\css\authors.css" "%~dp0..\css\methods.css" "%~dp0..\css\stock.css" "%~dp0..\css\online.css" "%~dp0..\css\forms.css" "%~dp0..\css\master.css" "%~dp0..\css\offline.css" "%~dp0..\css\history.css" "%~dp0..\css\why.css" "%~dp0..\css\faq.css" "%~dp0..\css\footer.css" "%~dp0..\css\popups.css" "%~dp0..\css\menu.css" "%~dp0..\css\tablet.css" "%~dp0..\css\mobile.css" "%~dp0..\css\fix.css" > styles.css
