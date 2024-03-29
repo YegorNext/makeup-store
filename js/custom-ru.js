ed21271bb6162c532453bc4d46c23e30baeb4d73
YegorNext <yegor.d2015@gmail.com>
2024-03-29 17:30:55 +0200

    ru-final localizaton

---
+++ js/custom-ru.js
@@ -0,0 +1,374 @@
+$('#spoiler-ph-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    
+
+    if(expanded == 'true') $('#switcher1').addClass('expand');
+    
+    if(expanded == 'false') $('#switcher1').removeClass('expand');
+
+});
+$('#spoiler-lk-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    
+
+    if(expanded == 'true') $('#switcher2').addClass('expand');
+    
+    if(expanded == 'false') $('#switcher2').removeClass('expand');
+
+});
+$('#spoiler-wm-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    
+
+    if(expanded == 'true') $('#switcher3').addClass('expand');
+    
+    if(expanded == 'false') $('#switcher3').removeClass('expand');
+
+});
+
+$('#spoiler-sp-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    
+
+    if(expanded == 'true') $('#switcher4').addClass('expand');
+    
+    if(expanded == 'false') $('#switcher4').removeClass('expand');
+
+});
+
+function stopAllVideos(){
+     $('video:not([autoplay])').each(function() {
+          this.pause();
+     });   
+}
+
+$('#spoiler-st-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-dbl').addClass('expand');
+         
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-dbl').removeClass('expand');
+
+         video.pause();
+    }
+
+});
+
+
+$('#spoiler-lp-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-lp').addClass('expand');
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-lp').removeClass('expand');
+
+         video.pause();
+    }
+
+});
+
+$('#spoiler-one-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-one').addClass('expand');
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-one').removeClass('expand');
+
+         video.pause();
+    }
+
+});
+
+$('#spoiler-two-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-two').addClass('expand');
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-two').removeClass('expand');
+
+         video.pause();
+    }
+
+});
+
+$('#spoiler-three-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-three').addClass('expand');
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-three').removeClass('expand');
+
+         video.pause();
+    }
+
+});
+
+$('#spoiler-four-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-four').addClass('expand');
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-four').removeClass('expand');
+
+         video.pause();
+    }
+
+});
+
+$('#spoiler-five-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-five').addClass('expand');
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-five').removeClass('expand');
+
+         video.pause();
+    }
+
+});
+
+
+$('#spoiler-six-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-six').addClass('expand');
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-six').removeClass('expand');
+
+         video.pause();
+    }
+
+});
+
+$('#spoiler-seven-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-seven').addClass('expand');
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-seven').removeClass('expand');
+
+         video.pause();
+    }
+
+});
+
+$('#spoiler-eight-btn').on('click', function(){
+    var expanded = $(this).attr('aria-expanded');
+    var video = $(this).parent().find('video').get(0);
+
+    if(expanded == 'true'){
+         $(this).addClass('hide-angels');
+         $('#switcher-eight').addClass('expand');
+
+         stopAllVideos();
+         video.play();
+    }
+
+    if(expanded == 'false'){
+         $(this).removeClass('hide-angels');
+         $('#switcher-eight').removeClass('expand');
+         
+         video.pause();
+    }
+
+});
+
+$('#faq-first-btn').on('click', function(){
+     var expanded = $(this).attr('aria-expanded');
+     
+ 
+     if(expanded == 'true') $('#faq_switcher-first').addClass('expand');
+     
+     if(expanded == 'false') $('#faq_switcher-first').removeClass('expand');
+ 
+ });
+
+ $('#faq-second-btn').on('click', function(){
+     var expanded = $(this).attr('aria-expanded');
+     
+ 
+     if(expanded == 'true') $('#faq_switcher-second').addClass('expand');
+     
+     if(expanded == 'false') $('#faq_switcher-second').removeClass('expand');
+ 
+ });
+
+ $('#faq-third-btn').on('click', function(){
+     var expanded = $(this).attr('aria-expanded');
+     
+ 
+     if(expanded == 'true') $('#faq_switcher-third').addClass('expand');
+     
+     if(expanded == 'false') $('#faq_switcher-third').removeClass('expand');
+ 
+ });
+
+
+ $('#faq-fourth-btn').on('click', function(){
+     var expanded = $(this).attr('aria-expanded');
+     
+ 
+     if(expanded == 'true') $('#faq_switcher-fourth').addClass('expand');
+     
+     if(expanded == 'false') $('#faq_switcher-fourth').removeClass('expand');
+ 
+ });
+
+ $('#faq-fifth-btn').on('click', function(){
+     var expanded = $(this).attr('aria-expanded');
+     
+ 
+     if(expanded == 'true') $('#faq_switcher-fifth').addClass('expand');
+     
+     if(expanded == 'false') $('#faq_switcher-fifth').removeClass('expand');
+ 
+ });
+
+
+
+ 
+
+ function controlArrow(controller, element){
+     $(controller).on('click', function(){
+          var expanded = $(this).attr('aria-expanded');
+     
+          if(expanded == 'true' && !$(element).hasClass('show')){
+                $(element).addClass('show');
+                $(this).find('u').text('');
+          }
+          else{
+               $(element).removeClass('show');
+               $(this).find('u').text('Читать полный отзывы');
+         }
+     });
+
+     $(element).on('click', function(){          
+          $(controller).click();
+     });
+ }
+
+
+ controlArrow('#history-first-btn', '#history__arrow-first');
+ controlArrow('#history-second-btn', '#history__arrow-second');
+ controlArrow('#history-third-btn', '#history__arrow-third');
+ controlArrow('#history-fourth-btn', '#history__arrow-fourth');
+ controlArrow('#history-fifth-btn', '#history__arrow-fifth');
+ controlArrow('#history-six-btn', '#history__arrow-six');
+ controlArrow('#history-seven-btn', '#history__arrow-seven');
+ controlArrow('#history-eigth-btn', '#history__arrow-eigth');
+
+ 
+ controlArrow('#history-nine-btn', '#history__arrow-nine');
+ controlArrow('#history-ten-btn', '#history__arrow-ten');
+ controlArrow('#history-eleven-btn', '#history__arrow-eleven');
+ controlArrow('#history-twelve-btn', '#history__arrow-twelve');
+ controlArrow('#history-thirteen-btn', '#history__arrow-thirteen');
+ controlArrow('#history-fourteen-btn', '#history__arrow-fourteen');
+ controlArrow('#history-fifteen-btn', '#history__arrow-fifteen');
+ controlArrow('#history-sixteen-btn', '#history__arrow-sixteen');
+
+ controlArrow('#history-seventeen-btn', '#history__arrow-seventeen');
+ controlArrow('#history-eighteen-btn', '#history__arrow-eighteen');
+ controlArrow('#history-nineteen-btn', '#history__arrow-nineteen');
+ controlArrow('#history-twenty-btn', '#history__arrow-twenty');
+ controlArrow('#history-twentyone-btn', '#history__arrow-twentyone');
+ controlArrow('#history-twentytwo-btn', '#history__arrow-twentytwo');
+ controlArrow('#history-twentythree-btn', '#history__arrow-twentythree');
+ controlArrow('#history-twentyfour-btn', '#history__arrow-twentyfour');
+
+
+
+ const detailButtons = document.getElementsByClassName('stock__detail-btn');
+
+ for(let btn of detailButtons){
+     btn.addEventListener('click', function(){
+          if($(this).attr('aria-expanded') == 'true')
+               this.innerHTML = '<u class = "small white">скрыть</u>';
+          else this.innerHTML = '<u class = "small white">читать детально</u>';
+     });
+ }
---
+++ js/validator-ru.js
@@ -0,0 +1,106 @@
+
+function formCheck(formElement){
+    var isValid = true;
+
+    // required fields
+    var phone = formElement.find('input[name="phone"]');
+    var name = formElement.find('input[name="name"]');
+    
+    // additional fields
+    var instagram = formElement.find('input[name="inst"]');
+
+    // checkbox
+    var check_inst = formElement.find('input[name="check-inst"]');
+
+    // conditions
+    if(phone.val().length < 5){
+        phone.addClass('forms__error-input');
+        isValid = false;
+    } 
+
+    if(name.val().length < 2){
+         name.addClass('forms__error-input');
+         isValid = false;
+    }
+        
+    if(check_inst.prop('checked') && instagram.val().length < 2){
+        formElement.find('.forms__error-win').text('Напишите свой Instagram, чтобы мы связались с вами');
+        instagram.addClass('forms__error-input');
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+function formReset(formElement){
+    var fieldsToReset = ['phone', 'name', 'inst', 'comment'];
+    var checkboxesToUncheck = ['check-inst', 'check-tel', 'check-whats', 'check-viber'];
+
+    fieldsToReset.forEach(function(fieldName) {
+        formElement.find('input[name="' + fieldName + '"]').val('');
+    });
+
+    checkboxesToUncheck.forEach(function(checkboxName) {
+        formElement.find('input[name="' + checkboxName + '"]').prop('checked', false);
+    });
+}
+
+function Main(){
+    var forms = [ $('#firstForm'), $('#secondForm'), $('#modalForm') ];
+
+    for(let form of forms){
+        form.submit(function(event){
+            event.preventDefault();
+
+            
+
+            if(formCheck(form)){
+                let formData = {};
+		form.find('input').each(function() {
+                    var input = $(this);
+                    
+		    // Проверка, является ли поле чекбоксом
+    		    if (input.attr('type') === 'checkbox') {
+        	    // Добавление значения "on" к formData, если чекбокс отмечен
+        	         formData[input.attr('name')] = input.is(':checked') ? 'on' : 'off';
+    		    } else {
+        	      // Для остальных полей добавление обычного значения
+        	    formData[input.attr('name')] = input.val();
+ 		}
+                });
+
+                $.ajax({
+                    url: 'data-controller/controller.php', // Замените на путь к вашему серверному файлу
+                    type: 'POST',
+                    data: formData,
+                    success: function() {
+                       console.log("Form sended");
+
+                        $('#modalForm').modal('hide');
+                        $('#modalComplete').modal('show');
+
+                        formReset(form);
+                    },
+                    error: function(error) {
+                      console.log('Form error'  + ': ', error);
+                    }
+                });
+            }
+        });
+
+        let inputs = form.find('input');
+        for(let input of inputs){
+            $(input).on('click', function(){
+                $(input).removeClass('forms__error-input');
+            });
+        }
+
+        form.find('input[name="inst"]').on('click', function(){
+            form.find('.forms__error-win').text('');
+        });
+    }
+   
+
+}
+
+Main();
